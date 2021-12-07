ec4b5f8e521bbf680e29c33c9d80ff92679ed07a
Andrey Ezhov <ezhovandr@gmail.com>
2021-12-07 19:43:36 +0400

    Final architecture

--- fdtd-2d.c
+++ fdtd-2d.c
@@ -50,17 +50,17 @@ void init_array (int tmax,
 		mtype = FROM_MASTER;
 		MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
 		MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-		for (i = offset; i < offset + rows; i++)
+		for (i = 0; i < rows; i++)
 			for (j = 0; j < ny; j++)
 			{
-				ex[i][j] = ((double) i / nx) * (j+1);
-				ey[i][j] = ((double) i / ny) * (j+2);
-				hz[i][j] = ((double) i / ny) * (j+3);
+				ex[i][j] = ((double) (i + offset) / nx) * (j+1);
+				ey[i][j] = ((double) (i + offset) / ny) * (j+2);
+				hz[i][j] = ((double) (i + offset) / ny) * (j+3);
 			}
 		mtype = TO_MASTER;
-		MPI_Send(&ex[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-		MPI_Send(&ey[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-		MPI_Send(&hz[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
+		MPI_Send(&ex[0][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
+		MPI_Send(&ey[0][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
+		MPI_Send(&hz[0][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
 	}
 }
 
@@ -92,9 +92,8 @@ void kernel_fdtd_2d(
         	extra =  (nx - 1) % numworkers;
         	offset = 1;
         	mtype = FROM_MASTER;
-			for (dest = 1; dest < numworkers; dest++) {
+			for (dest = 1; dest <= numworkers; dest++) {
 				rows = (dest <= extra) ? averow + 1 : averow;
-				MPI_Send(&offset, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
 				MPI_Send(&rows, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
 				MPI_Send(&ex[offset][0], rows * ny, MPI_DOUBLE, dest, mtype, MPI_COMM_WORLD);
 				MPI_Send(&ey[offset][0], rows * ny, MPI_DOUBLE, dest, mtype, MPI_COMM_WORLD);
@@ -102,22 +101,14 @@ void kernel_fdtd_2d(
 				offset = offset + rows;
 			}
 
-			for (i = offset; i < offset + averow; i++)
-				for (j = 1; j < ny; j++) {
-					ex[i][j] -= 0.5*(hz[i][j]-hz[i][j-1]);
-					ey[i][j] -= 0.5*(hz[i][j]-hz[i-1][j]);
-				}
-
 			offset = 1;
 			mtype = TO_MASTER;
-			for (source = 1; source < numworkers; source++) {
+			for (source = 1; source <= numworkers; source++) {
 				rows = (source <= extra) ? averow + 1 : averow;
 				MPI_Recv(&ex[offset][0], rows * ny, MPI_DOUBLE, source, mtype, MPI_COMM_WORLD, &status);
 				MPI_Recv(&ey[offset][0], rows * ny, MPI_DOUBLE, source, mtype, MPI_COMM_WORLD, &status);
 				offset = offset + rows;
 			}
-
-			MPI_Barrier(MPI_COMM_WORLD);
 
 			if (DEBUG) {
 				printf("after first barrier\n");
@@ -128,9 +119,8 @@ void kernel_fdtd_2d(
 
 			offset = 0;
         	mtype = FROM_MASTER;
-			for (dest = 1; dest < numworkers; dest++) {
+			for (dest = 1; dest <= numworkers; dest++) {
 				rows = (dest <= extra) ? averow + 1 : averow;
-				MPI_Send(&offset, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
 				MPI_Send(&rows, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
 				MPI_Send(&ex[offset][0], rows * ny, MPI_DOUBLE, dest, mtype, MPI_COMM_WORLD);
 				MPI_Send(&ey[offset][0], (rows + 1) * ny, MPI_DOUBLE, dest, mtype, MPI_COMM_WORLD);
@@ -142,16 +132,9 @@ void kernel_fdtd_2d(
 				printf("after send in second block\n");
 			}
 
-			for (i = offset; i < offset + averow - 1; i++)
-				for (j = 0; j < ny - 1; j++)
-					hz[i][j] -= 0.7* (ex[i][j+1] - ex[i][j] + ey[i+1][j] - ey[i][j]);
-
-			if (DEBUG) {
-				printf("before recieve in second block\n");
-			}
 			offset = 0;
 			mtype = TO_MASTER;
-			for (source = 1; source < numworkers; source++) {
+			for (source = 1; source <= numworkers; source++) {
 				rows = (source <= extra) ? averow + 1 : averow;
 				MPI_Recv(&hz[offset][0], rows * ny, MPI_DOUBLE, source, mtype, MPI_COMM_WORLD, &status);
 				offset = offset + rows;
@@ -160,48 +143,40 @@ void kernel_fdtd_2d(
 			if (DEBUG) {
 				printf("end of iteration!\n\n\n");
 			}
-
-			MPI_Barrier(MPI_COMM_WORLD);
 		}
 
 		if (taskid > MASTER) {
 			mtype = FROM_MASTER;
-			MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
 			MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-			MPI_Recv(&ex[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
-			MPI_Recv(&ey[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
-			MPI_Recv(&hz[offset - 1][0], (rows + 1) * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+			MPI_Recv(&ex[1][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+			MPI_Recv(&ey[1][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+			MPI_Recv(&hz[0][0], (rows + 1) * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
 
-			for (i = offset; i < offset + rows; i++)
+			for (i = 1; i < rows + 1; i++)
 				for (j = 1; j < ny; j++) {
 					ex[i][j] -= 0.5*(hz[i][j]-hz[i][j-1]);
 					ey[i][j] -= 0.5*(hz[i][j]-hz[i-1][j]);
 				}
 
-			for (i = offset; i < offset + rows; i++)
+			for (i = 1; i < rows + 1; i++)
 				ey[i][0] -= 0.5*(hz[i][0]-hz[i-1][0]);
 			
 			mtype = TO_MASTER;
-			MPI_Send(&ex[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-			MPI_Send(&ey[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-
-			MPI_Barrier(MPI_COMM_WORLD);
+			MPI_Send(&ex[1][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
+			MPI_Send(&ey[1][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
 
 			mtype = FROM_MASTER;
-			MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
 			MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-			MPI_Recv(&ex[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
-			MPI_Recv(&ey[offset][0], (rows + 1) * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
-			MPI_Recv(&hz[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+			MPI_Recv(&ex[0][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+			MPI_Recv(&ey[0][0], (rows + 1) * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+			MPI_Recv(&hz[0][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
 
-			for (i = offset; i < offset + rows; i++)
+			for (i = 0; i < rows; i++)
 				for (j = 0; j < ny - 1; j++)
 					hz[i][j] -= 0.7 * (ex[i][j+1] - ex[i][j] + ey[i+1][j] - ey[i][j]);
 
 			mtype = TO_MASTER;
-			MPI_Send(&hz[offset][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-
-			MPI_Barrier(MPI_COMM_WORLD);
+			MPI_Send(&hz[0][0], rows * ny, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
 		}
 	}
 }
@@ -219,14 +194,21 @@ int main(int argc, char** argv) {
 	int tmax = TMAX;
 	int nx = NX;
 	int ny = NY;
-	double (*ex)[nx][ny];
-	ex = (double(*)[nx][ny])malloc ((nx) * (ny) * sizeof(double));
+	int maxrows;
+	if (taskid == MASTER) {
+		maxrows = nx;
+	}
+	else {
+		maxrows = (nx - 1) / (numtasks - 1) + 2;
+	}
+	double (*ex)[maxrows][ny];
+	ex = (double(*)[maxrows][ny])malloc ((maxrows) * (ny) * sizeof(double));
 
-	double (*ey)[nx][ny];
-	ey = (double(*)[nx][ny])malloc ((nx) * (ny) * sizeof(double));
+	double (*ey)[maxrows][ny];
+	ey = (double(*)[maxrows][ny])malloc ((maxrows) * (ny) * sizeof(double));
 
-	double (*hz)[nx][ny];
-	hz = (double(*)[nx][ny])malloc ((nx) * (ny) * sizeof(double));
+	double (*hz)[maxrows][ny];
+	hz = (double(*)[maxrows][ny])malloc ((maxrows) * (ny) * sizeof(double));
 	init_array (
 		tmax, nx, ny,
 		*ex,
@@ -246,7 +228,7 @@ int main(int argc, char** argv) {
 		*ex,
 		*ey,
 		*hz,
-	 	numtasks,
+	 	numtasks - 1,
 	 	taskid
 	);
 
